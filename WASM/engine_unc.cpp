859ab54be923ba5f9197a43bf9ff2d93933d709f
Utkarsh Naman <truly.away@gmail.com>
2026-01-09 18:00:42 +0530

    updated engine, because it had bad unfolding/ reverse engineer logic, also had to do again a new cdn3 folder cuz the jsdelivr cdn was taking too long to update.

--- WASM/engine_unc.cpp
+++ WASM/engine_unc.cpp
@@ -87,16 +87,16 @@ State_str UncEngine::sort_state(const State_str& state)
 
 string UncEngine::move_played(const State_str& og_state_i, State_str state_f)
 {
-    cout << state_f.data() <<endl;
+    cout << "next state by bot: " << state_f.data() <<endl;
     const State_str sorted_og = sort_state(og_state_i);
     Move_str move{};
     std::string move_str;
     move[2] = '\0';
-    const int rl_i = (static_cast<int>(sorted_og[0])-48);
-    const int rr_i = (static_cast<int>(sorted_og[1])-48);
-    const int ul_i = (static_cast<int>(sorted_og[2])-48);
-    const int ur_i = (static_cast<int>(sorted_og[3])-48);
-    const int attacker_i = (static_cast<int>(sorted_og[4])-48);
+    const int rl_i = (static_cast<int>(og_state_i[0])-48);
+    const int rr_i = (static_cast<int>(og_state_i[1])-48);
+    const int ul_i = (static_cast<int>(og_state_i[2])-48);
+    const int ur_i = (static_cast<int>(og_state_i[3])-48);
+    const int attacker_i = (static_cast<int>(og_state_i[4])-48);
 
     const int rl_f = (static_cast<int>(state_f[0])-48);
     const int rr_f = (static_cast<int>(state_f[1])-48);
@@ -114,34 +114,81 @@ string UncEngine::move_played(const State_str& og_state_i, State_str state_f)
             return move_str;
         }
 
+        State_str result_ll = {};
+        result_ll[0] = og_state_i[0];
+        result_ll[1] = og_state_i[1];
+        result_ll[2] = '0'+static_cast<short>((rl_i+ul_i)%5);
+        result_ll[3] = og_state_i[3];
+        result_ll[4] = '1';
+        result_ll = sort_state(result_ll);
 
-        if (ul_f == (ul_i+rl_i)%5 && ul_i != 0 && rl_i != 0) // r: l->l
+        State_str result_lr = {};
+        result_lr[0] = og_state_i[0];
+        result_lr[1] = og_state_i[1];
+        result_lr[2] = og_state_i[2];
+        result_lr[3] = '0'+static_cast<short>((rl_i+ur_i)%5);
+        result_lr[4] = '1';
+        result_lr = sort_state(result_lr);
+
+        State_str result_rl = {};
+        result_rl[0] = og_state_i[0];
+        result_rl[1] = og_state_i[1];
+        result_rl[2] = '0'+static_cast<short>((rr_i+ul_i)%5);
+        result_rl[3] = og_state_i[3];
+        result_rl[4] = '1';
+        result_rl = sort_state(result_rl);
+
+        State_str result_rr = {};
+        result_rr[0] = og_state_i[0];
+        result_rr[1] = og_state_i[1];
+        result_rr[2] = og_state_i[2];
+        result_rr[3] = '0'+static_cast<short>((rr_i+ur_i)%5);
+        result_rr[4] = '1';
+        result_rr = sort_state(result_rr);
+
+        if (state_f == result_ll)
         {
             move_str = "ll";
         }
-        else if (ul_f == (ul_i+rr_i)%5 && ul_i != 0 && rr_i != 0) // r: r->l
-        {
-            move_str = "rl";
-        }
-        else if (ur_f == (ur_i+rl_i)%5 && ur_i != 0 && rl_i != 0) // r: l->r
+        if (state_f == result_lr)
         {
             move_str = "lr";
         }
-        else if (ur_f == (ur_i+rr_i)%5 && ur_i != 0 && rr_i != 0) // r: r->r
+        if (state_f == result_rl)
+        {
+            move_str = "rl";
+        }
+        if (state_f == result_rr)
         {
             move_str = "rr";
-            return move_str;
         }
 
-        if (static_cast<int>(og_state_i[0]) > static_cast<int>(og_state_i[1]))
-        {
-            move_str[0] = (move_str[0] == 'l')? 'r': 'l';
-            if (static_cast<int>(og_state_i[2]) > static_cast<int>(og_state_i[3]))
-            {
-                move_str[1] = (move_str[1] == 'l')? 'r': 'l';
-            }
-        }
-
+        // if (ul_f == (ul_i+rl_i)%5 && ul_i != 0 && rl_i != 0) // r: l->l
+        // {
+        //     move_str = "ll";
+        // }
+        // else if (ul_f == (ul_i+rr_i)%5 && ul_i != 0 && rr_i != 0) // r: r->l
+        // {
+        //     move_str = "rl";
+        // }
+        // else if (ur_f == (ur_i+rl_i)%5 && ur_i != 0 && rl_i != 0) // r: l->r
+        // {
+        //     move_str = "lr";
+        // }
+        // else if (ur_f == (ur_i+rr_i)%5 && ur_i != 0 && rr_i != 0) // r: r->r
+        // {
+        //     move_str = "rr";
+        //     return move_str;
+        // }
+        //
+        // if (static_cast<int>(og_state_i[0]) > static_cast<int>(og_state_i[1]))
+        // {
+        //     move_str[0] = (move_str[0] == 'l')? 'r': 'l';
+        //     if (static_cast<int>(og_state_i[2]) > static_cast<int>(og_state_i[3]))
+        //     {
+        //         move_str[1] = (move_str[1] == 'l')? 'r': 'l';
+        //     }
+        // }
     }
 
     else
@@ -156,35 +203,83 @@ string UncEngine::move_played(const State_str& og_state_i, State_str state_f)
 
         // cout << " bot chance no dist" <<endl;
 
-        if (rl_f == (rl_i+ul_i)%5 && rl_i != 0 && ul_i != 0) // u: l->l
+        State_str result_ll = {};
+        result_ll[0] = '0'+static_cast<short>((rl_i+ul_i)%5);
+        result_ll[1] = og_state_i[1];
+        result_ll[2] = og_state_i[2];
+        result_ll[3] = og_state_i[3];
+        result_ll[4] = '0';
+        result_ll = sort_state(result_ll);
+
+        State_str result_lr = {};
+        result_lr[0] = og_state_i[0];
+        result_lr[1] = '0'+static_cast<short>((rr_i+ul_i)%5);
+        result_lr[2] = og_state_i[2];
+        result_lr[3] = og_state_i[3];
+        result_lr[4] = '0';
+        result_lr = sort_state(result_lr);
+
+        State_str result_rl = {};
+        result_rl[0] = '0'+static_cast<short>((rl_i+ur_i)%5);
+        result_rl[1] = og_state_i[1];
+        result_rl[2] = og_state_i[2];
+        result_rl[3] = og_state_i[3];
+        result_rl[4] = '0';
+        result_rl = sort_state(result_rl);
+
+        State_str result_rr = {};
+        result_rr[0] = og_state_i[0];
+        result_rr[1] = '0'+static_cast<short>((rr_i+ur_i)%5);
+        result_rr[2] = og_state_i[2];
+        result_rr[3] = og_state_i[3];
+        result_rr[4] = '0';
+        result_rr = sort_state(result_rr);
+
+        if (state_f == result_ll)
         {
             move_str = "ll";
         }
-        else if (rl_f == (rl_i+ur_i)%5 && rl_i != 0 && ur_i != 0) // u: r->l
-        {
-            move_str = "rl";
-        }
-        else if (rr_f == (rr_i+ul_i)%5 && rr_i != 0 && ul_i != 0) // u: l->r
+        if (state_f == result_lr)
         {
             move_str = "lr";
         }
-        else if (rr_f == (rr_i+ur_i)%5 && rr_i != 0 && ur_i != 0) // u: l->r
+        if (state_f == result_rl)
+        {
+            move_str = "rl";
+        }
+        if (state_f == result_rr)
         {
             move_str = "rr";
         }
 
-        // cout << "move str before reverse engineering the sortion" << move_str.data() << endl;
-        if (static_cast<int>(og_state_i[0]) > static_cast<int>(og_state_i[1]))
-        {
-            move_str[1] = (move_str[1] == 'l')? 'r': 'l';
-            if (static_cast<int>(og_state_i[2]) > static_cast<int>(og_state_i[3]))
-            {
-                move_str[0] = (move_str[0] == 'l')? 'r': 'l';
-            }
-        }
+
+        // if (rl_f == (rl_i+ul_i)%5 && rl_i != 0 && ul_i != 0) // u: l->l
+        // {
+        //     move_str = "ll";
+        // }
+        // else if (rl_f == (rl_i+ur_i)%5 && rl_i != 0 && ur_i != 0) // u: r->l
+        // {
+        //     move_str = "rl";
+        // }
+        // else if (rr_f == (rr_i+ul_i)%5 && rr_i != 0 && ul_i != 0) // u: l->r
+        // {
+        //     move_str = "lr";
+        // }
+        // else if (rr_f == (rr_i+ur_i)%5 && rr_i != 0 && ur_i != 0) // u: l->r
+        // {
+        //     move_str = "rr";
+        // }
+        //
+        // // cout << "move str before reverse engineering the sortion" << move_str.data() << endl;
+        // if (static_cast<int>(og_state_i[0]) > static_cast<int>(og_state_i[1]))
+        // {
+        //     move_str[1] = (move_str[1] == 'l')? 'r': 'l';
+        //     if (static_cast<int>(og_state_i[2]) > static_cast<int>(og_state_i[3]))
+        //     {
+        //         move_str[0] = (move_str[0] == 'l')? 'r': 'l';
+        //     }
+        // }
     }
-
-
 
     return move_str;
 }
